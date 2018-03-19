794534c1d32a47ccdc9530aaa03bad8cdfb27292
yawows111 <yawows111@gmail.com>
2018-03-19 19:19:42 +0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import telebot
+bot = telebot.TeleBot('256863519:AAF9kKEoSBjo2DL6i51-VyJImA0_1ZQII4s') #объект бот
+
+@bot.message_handler(commands=['start','help'])
+def handle_start(message):
+
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, True)
+    user_markup.row('Меню')
+    user_markup.row('Информация о Боте.')
+
+
+    bot.send_message(message.from_user.id, 'Добро пожаловать..', reply_markup=user_markup)
+bot.polling(none_stop=True)  # обработка функций
